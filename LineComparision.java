6d93ab10004cce589e009d6fb09b93786b656d97
biswajitdixit <buntyoec1998@gmail.com>
2021-01-22 20:16:18 +0530

    ADDED:The length calculation

--- LineComparision.java
+++ LineComparision.java
@@ -1,7 +1,16 @@
 public class LineComparision{
+ static final int x1=2;
+static final int x2=4;
+static final int y1=5;
+static final int y2=7;
 public static void main(String[]args){
 System.out.println("Welcome To Line Comparision Computation");
 
- }
+  double length1=0;
+  double x=Math.pow((x2-x1),2);
+  double y=Math.pow((y2-y1),2);
+  length1 = Math.sqrt(x+y);
+  System.out.println("length of the line: "+  length1);
+}
 }
 
